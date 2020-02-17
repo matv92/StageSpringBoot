c947f17267e1c7b7f074feab51a09973894d5ead
mattia <mattia.valli@farnetworks.com>
2020-02-17 16:11:06 +0100

    primo commit

---
+++ auth-service/src/test/java/com/pmpay/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pmpay.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import it.portit.authservice.AuthServiceApplication;
+
+@SpringBootTest( classes = AuthServiceApplication.class)
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
