947461161ef4d8f231f1241b43304a2f06cc3d47
mattia <mattia.valli@farnetworks.com>
2020-02-18 14:21:14 +0100

    Enable Spring admin

--- napier-service/src/main/java/it/portit/napier/NapierServiceApplication.java
+++ napier-service/src/main/java/it/portit/napier/NapierServiceApplication.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient 		// Enable eureka client.
+@EnableEurekaClient
 @EnableHystrixDashboard
 @EnableCircuitBreaker
 public class NapierServiceApplication {
