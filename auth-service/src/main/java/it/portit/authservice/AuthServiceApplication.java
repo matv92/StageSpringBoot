3f617c3ba3c3501f6bf72099d43b1d4d92c9a410
mattia <mattia.valli@farnetworks.com>
2020-02-18 14:12:01 +0100

    Enable spring server admin

--- auth-service/src/main/java/it/portit/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/it/portit/authservice/AuthServiceApplication.java
@@ -2,12 +2,10 @@ package it.portit.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
